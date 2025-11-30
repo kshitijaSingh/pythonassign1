50f1d280988ad37e71271b2a0a02cf01d2f659d9
kshitijaSingh <kshitijasingh1106@gmail.com>
2025-11-30 23:41:40 +0530

    initial commit

---
+++ assignmnet 1.py
@@ -0,0 +1,32 @@
+"""
+task 1 :
+a = number 1
+b = number 2
+addition = a +b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+"""
+
+a = int(input("enter your number 1:"))
+b = int(input("enter your number 2:"))
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print("number 1 :", a )
+print(" number 2",  b )
+print(" Addition :", add)
+print(" Subtraction :", sub)
+print(" Multiplication :", mul)
+print(" Division :", div)
+
+"""
+task 2 :
+first name = FN
+last name = LN
+hello, FN LN ! welcome to the python programme
+"""
+FN = input("enter your first name:")
+LN = input("enter your last name:")
+print("hello,", FN, LN ,"! welcome to the python programme")
